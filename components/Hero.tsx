290401b02487f9346dae6199f7bdce194ecbc9b2
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-01-03 07:31:33 +0000

    added framer motion

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { motion } from "motion/react";
+
 export default function Hero() {
     return (
-        <div>
-            <h1>Hero</h1>
-        </div>
+        <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            <motion.h1
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+            >
+                Hero
+            </motion.h1>
+        </motion.div>
     );
 }
