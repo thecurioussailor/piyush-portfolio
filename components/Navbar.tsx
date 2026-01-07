92dfb3be736b98970931aa7086a8f7e9452ab482
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-01-07 18:22:30 +0000

    added navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="pt-12">
+      <div className="flex justify-between items-center">
+        <header className="flex items-center justify-center w-full">
+          <div className="relative border-10 border-black px-[40px] py-[50px]">
+            <div className="w-[10px] h-[10px] bg-white absolute -top-3 -left-3"></div>
+            <div></div>
+            <a>Piyush</a>
+            <div>menu icon</div>
+          </div>
+          <div></div>
+        </header>
+      </div>
     </div>
   );
 }
