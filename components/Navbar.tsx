0826ad0e2fbd9d8daf324f439a0ef4cd5204ea4b
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-01-10 18:20:07 +0000

    added animation to menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,21 @@
+"use client";
 import Image from "next/image";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { motion } from "motion/react";
+import { useState } from "react";
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex items-center justify-center py-12">
       <div className="flex items-center justify-center w-full max-w-[704px]">
         <header className="flex flex-col items-center justify-center w-full">
-          <div className="relative border-10 border-b-0 border-black px-[40px] py-[50px] w-full flex flex-row items-center justify-between">
+          <div className="relative border-10 border-b-0 border-black bg-white px-[40px] py-[50px] w-full flex flex-row items-center justify-between">
             <div className="w-[10px] h-[10px] bg-[#e7e8d3] absolute -top-[10px] -left-[10px]"></div>
             <div className="w-[10px] h-[10px] bg-[#e7e8d3] absolute -top-[10px] -right-[10px]"></div>
             <a className="flex flex-row items-center gap-[30px] w-full">
               <div className="rounded-full overflow-hidden border-2 border-black w-[100px] h-[100px]">
-                <Image src="/images/billa.png" alt="logo" width={100} height={100} className="object-cover object-center" />
+                <Image src="/images/billa2.png" alt="logo" width={100} height={100} className="object-cover object-center" />
               </div>
               <div>
                 <div>
@@ -22,9 +26,44 @@ export default function Navbar() {
                 </div>
               </div>
             </a>
-            <div>menu icon</div>
+            <div 
+              className="flex flex-col relative w-10 h-10 gap-2 cursor-pointer z-10 overflow-hidden"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <motion.div 
+                className="h-[2px] w-[32px] bg-black" 
+                animate={{
+                  rotate: isOpen ? 45 : 0,
+                  y: isOpen ? 10 : 0,
+                }}
+                transition={{duration: 0.3, ease: "easeInOut"}}
+              />
+              <motion.div 
+                className="h-[3px] w-[32px] bg-black" 
+                animate={{
+                  opacity: isOpen ? 0 : 1,
+                  scale: isOpen ? 0.5 : 1,
+                }}
+                transition={{duration: 0.2, ease: "easeInOut"}}
+              />
+              <motion.div 
+                className="h-[2px] w-[32px] bg-black" 
+                animate={{
+                  rotate: isOpen ? -45 : 0,
+                  y: isOpen ? -10 : 0,
+                }}
+                transition={{duration: 0.3, ease: "easeInOut"}}
+              />
+            </div>
+            <nav className="absolute top-44">
+                sakjskfh
+            </nav>
           </div>
-          <div className="border-x-10 border-[#e7e8d3] px-[40px] py-[6px] w-full bg-black flex items-center justify-between">
+          <div className="relative border-x-10 border-[#e7e8d3] px-[40px] py-[6px] w-full bg-black flex items-center justify-between">
+            <div className="w-[10px] h-[10px] bg-[#e7e8d3] absolute bottom-[10px] left-0"></div>
+            <div className="w-[10px] h-[10px] bg-[#e7e8d3] absolute bottom-0 left-[10px]"></div>
+            <div className="w-[10px] h-[10px] bg-[#e7e8d3] absolute bottom-[10px] right-[10px]"></div>
+            <div className="w-[10px] h-[10px] bg-[#e7e8d3] absolute bottom-0 right-0"></div>
             <div className="flex items-center gap-2">
               <div className="bg-[#05FF50] w-[10px] h-[10px] rounded-full"></div>
               <div>
